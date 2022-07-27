d68e8f8572ddd4301fecadb5fb533f8868f85301
Mrbysco <Mrbysco@users.noreply.github.com>
2022-07-28 00:37:47 +0200

    Update forge [push]

--- src/main/java/com/mrbysco/telepass/util/PlayerUtil.java
+++ src/main/java/com/mrbysco/telepass/util/PlayerUtil.java
@@ -4,17 +4,10 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
-import java.util.List;
 
 public class PlayerUtil {
 	@Nullable
 	public static Player getPlayerEntityByName(Level worldIn, String name) {
-		List<? extends Player> playerEntities = worldIn.players();
-		for (Player playerEntity : playerEntities) {
-			if (name.equals(playerEntity.getName().getContents())) {
-				return playerEntity;
-			}
-		}
-		return null;
+		return worldIn.players().stream().filter(player -> player.getName().getString().equals(name)).findFirst().orElse(null);
 	}
 }
